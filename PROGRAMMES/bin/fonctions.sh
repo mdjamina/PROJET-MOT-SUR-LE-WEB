fd2c64d3bc8ac1328c9968820f3df8ddeae38c4b
mdjamina <m.djarfi.amina@gmail.com>
2022-01-06 22:22:25 +0100

    corrections dump-text

--- PROGRAMMES/bin/fonctions.sh
+++ PROGRAMMES/bin/fonctions.sh
@@ -99,13 +99,9 @@ function detection_encodage(){
             #encodage=$(cat $NOM_FIC_PA | tr '/>' '\n'| egrep -m 1 'charset=' | cut -d"=" -f2 | tr -d '\r"' | tr '[a-z]' '[A-Z]')
         fi
 
-        #encodage=""
+        #ENCODAGE=""
 
-        if [[ $ENCODAGE == "UTF-8" ]]
-        then
-            CHECK_ENCODAGE=1;
-            NOM_FIC_PA_UTF8=$NOM_FIC_PA;
-        fi
+        [[ $ENCODAGE == "UTF-8" ]] && CHECK_ENCODAGE=1
 
         if [[ $ENCODAGE != "" ]] 
         then
@@ -120,30 +116,40 @@ function detection_encodage(){
 function gestion_encodage(){
     #echo $encodage | hex
     #logInfo "check_encodage:$CHECK_ENCODAGE"
-
-    if [[ $CHECK_ENCODAGE == 0 ]]
+    if [[ $NOM_FIC_DT != "" ]]
     then
-        #logInfo "-----Gestion d'encodage-----"
-        
-        if [[ $(iconv -l | grep $ENCODAGE) == "" ]]
+
+        if [[ $CHECK_ENCODAGE == 0 ]]
         then
-            log_failure "L'encodage '$ENCODAGE' non supporté";
+            #logInfo "-----Gestion d'encodage-----"
+            
+            if [[ $(iconv -l | grep $ENCODAGE) == "" ]]
+            then
+                log_failure "L'encodage '$ENCODAGE' non supporté";
+            else
+                NOM_FIC_DT_UTF8=$(echo $NOM_FIC_DT | sed "s/.txt/_utf8.txt/g") 
+                CHECK_ENCODAGE=1
+
+                #echo "NOM_FIC_PA=$NOM_FIC_PA"
+                #echo "NOM_FIC_PA_UTF8=$NOM_FIC_PA_UTF8"
+
+                iconv --from $ENCODAGE --to UTF-8 $NOM_FIC_DT >$NOM_FIC_DT_UTF8 2>$REP_LOG/command.log\
+                                    && log_success "conversion UTF-8" || log_failure "conversion UTF-8"`cat $REP_LOG/command.log`;
+
+                [[ $(check_error) == 0 ]] || CHECK_ENCODAGE=-1
+                #touche $REP_LOG/command.log
+            fi
         else
-            NOM_FIC_PA_UTF8=$(echo $NOM_FIC_PA | sed "s/.html/_UTF8.html/g") 
-            CHECK_ENCODAGE=1
+            NOM_FIC_DT_UTF8=$NOM_FIC_DT 
 
-            #echo "NOM_FIC_PA=$NOM_FIC_PA"
-            #echo "NOM_FIC_PA_UTF8=$NOM_FIC_PA_UTF8"
-
-            iconv --from $ENCODAGE --to UTF-8 $NOM_FIC_PA >$NOM_FIC_PA_UTF8 2>$REP_LOG/command.log\
-                                && log_success "conversion UTF-8" || log_failure "conversion UTF-8"`cat $REP_LOG/command.log`;
-
-            [[ $(check_error) == 0 ]] || CHECK_ENCODAGE=-1
-            #touche $REP_LOG/command.log
+            #logInfo "----------------------------"
         fi
 
-        #logInfo "----------------------------"
+        
     fi
+
+    #NOM_FIC_DT_UTF8=""
+
 
     #logInfo $NOM_FIC_PA_UTF8
     #logInfo $NOM_FIC_PA
@@ -153,17 +159,17 @@ function gestion_encodage(){
 
 function dump_text(){
 
-    if [[ $CHECK_ENCODAGE == 1 ]] && [[ $NOM_FIC_PA_UTF8 != "" ]]
+    if [[ $ENCODAGE != "" ]] 
     then
         #logInfo "--------Dump to Text--------"
 
-        NOM_FIC_DT=$REP_DT"/utf8_"$CPT_TABLE"_"$CPT_URL.txt
+        NOM_FIC_DT=$REP_DT"/"$CPT_TABLE"_"$CPT_URL.txt
 
         #echo "NOM_FIC_PA_UTF8=$NOM_FIC_PA_UTF8"
 
 
         #dumper le contenu de la page aspirée
-        lynx -dump -nolist -assume_charset="$ENCODAGE" -display_charset="UTF-8" $NOM_FIC_PA > $NOM_FIC_DT 2>$REP_LOG/command.log \
+        lynx -dump -nolist -assume_charset="$ENCODAGE" -display_charset="$ENCODAGE" $NOM_FIC_PA > $NOM_FIC_DT 2>$REP_LOG/command.log \
         && log_success "dumper le contenu de la page" || log_failure "lynx Error:"`cat $REP_LOG/command.log`;
 
         [[ $(check_error) == 0 ]] || NOM_FIC_DT=""
@@ -172,12 +178,15 @@ function dump_text(){
         
         #logInfo "----------------------------"
     fi
+
+    #NOM_FIC_DT=""
+    
 }
 
 
 function extraire_contextes(){
 
-    if [[ $NOM_FIC_DT != "" ]]
+    if [[ $NOM_FIC_DT_UTF8 != "" ]]
     then
         #logInfo "--------Dump to Text--------"
 
@@ -187,7 +196,7 @@ function extraire_contextes(){
                     # 3ème traitement : extraire des contextes réduits au motif (1 ligne avant et 1 ligne après)
 				# 2 méthodes : egrep + mingrep
 				# 1. construire des morceaux de corpus
-		egrep -C 2 -i "$MOTIFS" $NOM_FIC_DT > $NOM_FIC_CT 2>$REP_LOG/command.log \
+		egrep -C 2 -i "$MOTIFS" $NOM_FIC_DT_UTF8 > $NOM_FIC_CT 2>$REP_LOG/command.log \
         && log_success "Extraction contextes" || log_failure "Extraction contextes Error:"`cat $REP_LOG/command.log`;
 
         [[ $(check_error) == 0 ]] || NOM_FIC_CT=""
@@ -198,9 +207,9 @@ function extraire_contextes(){
 
 function comptage_motifs(){
 
-    if [[ $NOM_FIC_DT != "" ]]
+    if [[ $NOM_FIC_DT_UTF8 != "" ]]
     then
-		COMPTEUR_MOTIFS=$(egrep -o -i $MOTIFS $NOM_FIC_DT 2>$REP_LOG/command.log | wc -l);
+		COMPTEUR_MOTIFS=$(egrep -o -i $MOTIFS $NOM_FIC_DT_UTF8 2>$REP_LOG/command.log | wc -l);
 
 
         [[ $(check_error) == 0 ]] && log_success "Compteur motifs" || log_failure "Compteur motifs Error:"`cat $REP_LOG/command.log` ;
@@ -214,7 +223,7 @@ function comptage_motifs(){
 
 function calcul_index(){
 
-    if [[ $NOM_FIC_DT != "" ]]
+    if [[ $NOM_FIC_DT_UTF8 != "" ]]
     then
 		
         NOM_FIC_FW=$REP_DT"/index_"$CPT_TABLE"_"$CPT_URL.txt
@@ -223,7 +232,7 @@ function calcul_index(){
         #egrep -i -o "\w+" $NOM_FIC_DT | sort | uniq -c  | sort -r -n -s -k 1,1 > $NOM_FIC_FW 2>$REP_LOG/command.log \
          #       && log_success "Calcul index" || log_failure "Calcul index Error:"`cat $REP_LOG/command.log`
 
-        python3 ./bin/micro_nlp_utils.py 	-i $NOM_FIC_DT --get=index >$NOM_FIC_FW 2>$REP_LOG/command.log \
+        python3 ./bin/micro_nlp_utils.py 	-i $NOM_FIC_DT_UTF8 --get=index >$NOM_FIC_FW 2>$REP_LOG/command.log \
                     && log_success "Calcul index" || log_failure "Calcul index:"`cat $REP_LOG/command.log` ;
 
         [[ $(check_error) == 0 ]] || NOM_FIC_FW=""
@@ -234,7 +243,7 @@ function calcul_index(){
 
 function calcul_bigramme(){
 
-    if [[ $NOM_FIC_DT != "" ]]
+    if [[ $NOM_FIC_DT_UTF8 != "" ]]
     then
 		
         NOM_FIC_BG=$REP_DT"/bigramme_"$CPT_TABLE"_"$CPT_URL.txt
@@ -243,7 +252,7 @@ function calcul_bigramme(){
         #egrep -i -o "\w+" $NOM_FIC_DT | sort | uniq -c  | sort -r -n -s -k 1,1 > $NOM_FIC_FW 2>$REP_LOG/command.log \
          #       && log_success "Calcul index" || log_failure "Calcul index Error:"`cat $REP_LOG/command.log`
 
-        python3 ./bin/micro_nlp_utils.py 	-i $NOM_FIC_DT --get=bigrams >$NOM_FIC_BG 2>$REP_LOG/command.log \
+        python3 ./bin/micro_nlp_utils.py 	-i $NOM_FIC_DT_UTF8 --get=bigrams >$NOM_FIC_BG 2>$REP_LOG/command.log \
                     && log_success "Calcul bigramme" || log_failure "Calcul bigramme:"`cat $REP_LOG/command.log` ;
 
         [[ $(check_error) == 0 ]] || NOM_FIC_BG=""
@@ -289,10 +298,10 @@ function html_table_rows(){
             <td align=\"center\">---</td>";
     fi
 
-    if [[ $NOM_FIC_DT != "" ]]
+    if [[ $NOM_FIC_DT_UTF8 != "" ]]
     then 
         echo "
-            <td align=\"center\"><a href=\"$NOM_FIC_DT\">$CPT_TABLE"_"$CPT_URL</a></td>";
+            <td align=\"center\"><a href=\"$NOM_FIC_DT_UTF8\">$CPT_TABLE"_"$CPT_URL</a></td>";
     else
         echo "
             <td align=\"center\">---</td>";
--- PROGRAMMES/script_final.sh
+++ PROGRAMMES/script_final.sh
@@ -40,9 +40,9 @@ REP_DT=../DUMP-TEXT
 REP_CT=../CONTEXTES
 
 #Vidange des répertoires
-rm $REP_PA/*.html >$REP_LOG/command.log 2>&1 && log_success "Purge: $REP_PA" || log_failure "Error:"`cat $REP_LOG/command.log`
-rm $REP_DT/*.txt >$REP_LOG/command.log 2>&1 && log_success "Purge: $REP_DT" || log_failure "Error:"`cat $REP_LOG/command.log`
-rm $REP_CT/*.txt >$REP_LOG/command.log 2>&1 && log_success "Purge: $REP_CT" || log_failure "Error:"`cat $REP_LOG/command.log`
+[ "$(ls -A $REP_PA)" ] && rm $REP_PA/*.html >$REP_LOG/command.log 2>&1 && log_success "Purge: $REP_PA" || log_failure "Error:"`cat $REP_LOG/command.log`
+[ "$(ls -A $REP_DT)" ] && rm $REP_DT/*.txt >$REP_LOG/command.log 2>&1 && log_success "Purge: $REP_DT" || log_failure "Error:"`cat $REP_LOG/command.log`
+[ "$(ls -A $REP_CT)" ] && rm $REP_CT/*.txt >$REP_LOG/command.log 2>&1 && log_success "Purge: $REP_CT" || log_failure "Error:"`cat $REP_LOG/command.log`
 #exit 0
 #-------------------------------------------------------
 # en-tête du fichier html
@@ -102,19 +102,18 @@ do
             #détection de l'ENCODAGE    
             detection_encodage;
 
+            #
+            dump_text;
+
             # si encodage = UTF8 alors on fait certains traitements
 				# sinon il faudra aussi les faire et probablement d'autres
             gestion_encodage;
-
-            #
-            dump_text;
 
             #
             comptage_motifs;
 
             #
             extraire_contextes;
-
 
             #
             calcul_index;
